1892a59b34744d7f31fd631f66f6e8bac743aff4
Jan <j.schiff96@gmail.com>
2020-12-01 12:13:56 +0100

    introducing new functions for task 1

--- lookback option.py
+++ lookback option.py
@@ -19,63 +19,24 @@ Compare to European Put
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-
 
 
 # Fixed for Task 1:
 n = 50
 T = 1
 p = 0.5
-alpha = 0.1
-sigma = 0.5
+alpha_s = 0.1
+sigma_s = 0.5
 S_0 = 10
 N = 100
+r = 0.01
 
 # Fixed for Task 2:
+M = int(10)
 N = 100
-M = 10E5
 
 
-def RandomPathsBinomial(S, M):
-    N = len(S)-1
-    r = np.random.randint(0,2,size=(M,N))
-    Nu = np.sum(r==0,axis=1)
-    Rp = np.zeros((M,N+1))
-    rows = np.zeros((M,N+1))
-    rows[:,0] = 0
-    Rp[:,0] = S[0,0]
-    for j in range(N):
-        rows[:,j+1] = rows[:,j] + r[:,j]
-        Rp[:,j+1]= S[np.int_(rows[:,j+1]),j+1]
-    return Rp,Nu
-
-
-def BinomialLookback(Q,S,r,M):
-    h = Q[2]-Q[1]
-    N = len(Q)-1
-    expu = S[0,1]/S[0,0]
-    expd = S[1,1]/S[0,0]  #goes down rate
-    qu = (np.exp(r*h)-expd)/(expu-expd)
-    qd = (expu-np.exp(r*h))/(expu-expd)
-    if (qu<0) | (qd<0):
-        print('Error: the market is not arbitrage free.')
-        price = 0
-        return
-    [R,Nu] = RandomPathsBinomial(S,M)      #R is M*(N+1), Nu is size M vector
-    payoff = np.max(R, axis = 1)- R[:,N]
-    terms = (qu**Nu)*(qd**(N-Nu))*payoff
-    price = np.exp(-r*h*N)*sum(terms)*2**N/M    #monte carlo:2^N the possible paths. M samples
-    return price
-
-# #######
-# Task 1
-# #######
-
-def generate_S(T, N, S_0, alpha, sigma, p):
-    h = T/N
-    e_u = np.exp(alpha*h + sigma*np.sqrt(h)*np.sqrt((1-p)/p))
-    e_d = np.exp(alpha*h - sigma*np.sqrt(h)*np.sqrt(p/(1-p)))
+def generate_s(N, S_0, e_u, e_d):
     N += 1
     S_layer = np.zeros([N, N])
     S_layer[0, 0] = S_0
@@ -86,29 +47,84 @@ def generate_S(T, N, S_0, alpha, sigma, p):
     return S_layer
 
 
+def generate_paths(S_0, M, N, e_u, e_d):
+    instructions = np.random.choice((e_u, e_d), (M, N))
+    stock_price = np.zeros((M, N))
+    stock_price[:, 0] = S_0
+    for i in range(1, N):
+        slice = instructions[:, i]
+        stock_price[:, i] = stock_price[:, i-1] * slice
+
+    return stock_price
+
+
+def generate_payoff(stock):
+    maximum = np.max(stock, axis=1)
+    strike_price = stock[:, -1]
+    return maximum - strike_price
+
+# #######
+# Task 1
+# #######
+
+h = T/N
+e_u = np.exp(alpha_s*h + sigma_s*np.sqrt(h)*np.sqrt((1-p)/p))
+e_d = np.exp(alpha_s*h - sigma_s*np.sqrt(h)*np.sqrt(p/(1-p)))
+
 # fill out S
-S_Layer = generate_S(T, N, S_0, alpha, sigma, p)
 timesteps = np.arange(0, 1 + T/N, T/N)
 
-S = S_Layer.copy()
-Q = timesteps.copy()
+mean_store = []
+error_store = []
+
+m_values = [k for k in range(100, 100000, 10000)]
+for m in m_values:
+    mean = 0
+    error = 0
+    for i in range(n):
+        stock_paths = generate_paths(S_0, m, N, e_u, e_d)
+        payoff_paths = generate_payoff(stock_paths)
+
+        current_mean = np.mean(payoff_paths)
+        mean += current_mean
+        current_error = np.sqrt( 1/(n-1) * np.sum((payoff_paths - current_mean)**2) )/np.sqrt(n)
+        error += current_error
+
+    mean_store.append(mean/n)
+    error_store.append(error/n)
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+fig.suptitle('Task 1')
+ax1.set(title='Mean',
+        xlabel='M', ylabel='mean',
+        yscale='linear')
+ax1.label_outer()
+ax2.set(title='Standard Error',
+        xlabel='M', ylabel='Standard Error',
+        yscale='linear')
+ax2.label_outer()
+ax1.plot(mean_store)
+ax2.plot(error_store)
+plt.show()
+
+
 
 #Rand,Nu = RandomPathsBinomial(S,50)
 
 #plt.plot(np.transpose(Rand)) #as the arrays are column vectors not row
 #plt.show()
 
-P1 = np.zeros(100)
-P2 = np.zeros(100)
-P3 = np.zeros(100)
-
-for i in range(100):
-    P1[i] = BinomialLookback(Q,S,0.01,100)
-    P2[i] = BinomialLookback(Q,S,0.01,1000)
-    P3[i] = BinomialLookback(Q,S,0.01,10000)
-
-plt.figure(2)
-plt.plot(P1)
-plt.plot(P2)
-plt.plot(P3)
-plt.show()
+# P1 = np.zeros(100)
+# P2 = np.zeros(100)
+# P3 = np.zeros(100)
+#
+# for i in range(100):
+#     P1[i] = BinomialLookback(Q,S,0.01,100)
+#     P2[i] = BinomialLookback(Q,S,0.01,1000)
+#     P3[i] = BinomialLookback(Q,S,0.01,10000)
+#
+# plt.figure(2)
+# plt.plot(P1)
+# plt.plot(P2)
+# plt.plot(P3)
+# plt.show()
