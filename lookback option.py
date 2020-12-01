62818eea68c569417b6acc8a5f95977758fdde05
Jan <j.schiff96@gmail.com>
2020-12-01 10:14:26 +0100

    added docstring removed idea from git refactoring

--- lookback option.py
+++ lookback option.py
@@ -1,26 +1,59 @@
+"""
+Options and mathematics Project
+Lookback Put Derivative
+
+
+Task 1:
+Influence on Err and Mean
+    of M
+
+Task 2:
+Influences on Pi_y(0)
+    of p, alpha, sigma, r, T, S_0
+
+Task 3:
+Compare to European Put
+
+"""
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
+
+
+# Fixed for Task 1:
+n = 50
+T = 1
+p = 0.5
+alpha = 0.1
+sigma = 0.5
+S_0 = 10
+N = 100
+
+# Fixed for Task 2:
+N = 100
+M = 10E5
+
 
 def BinomialStock(p, alpha, sigma, s, T, N):
     h = T/N
     u = alpha*h + sigma*np.sqrt(h)*np.sqrt((1-p)/p)
     d = alpha*h - sigma*np.sqrt(h)*np.sqrt(p/(1-p))
     Q = np.zeros(N+1)
-    S = np.zeros((N+1,N+1))
+    S = np.zeros((N+1, N+1))
     Q[0] = 0
-    S[0,0] = s
+    S[0, 0] = s
     for j in range(N):
-        Q[j+1] = (j+1)*h
-        S[0,j+1] = S[0,j]*np.exp(u)
+        Q[j+1] = (j+1) * h
+        S[0, j+1] = S[0, j]*np.exp(u)
         for i in range(j+1):
-            S[i+1,j+1] = S[i,j]*np.exp(d)
-    return Q,S
-
-[Q,S]= BinomialStock(0.5,0.0,0.5,10,1,100)
+            S[i+1, j+1] = S[i, j]*np.exp(d)
+    return Q, S
 
 
-def RandomPathsBinomial(S,M):
+def RandomPathsBinomial(S, M):
     N = len(S)-1
     r = np.random.randint(0,2,size=(M,N))
     Nu = np.sum(r==0,axis=1)
@@ -33,11 +66,6 @@ def RandomPathsBinomial(S,M):
         Rp[:,j+1]= S[np.int_(rows[:,j+1]),j+1]
     return Rp,Nu
 
-#Rand,Nu = RandomPathsBinomial(S,50)
-
-#plt.plot(np.transpose(Rand)) #as the arrays are column vectors not row
-#plt.show()
-
 
 def BinomialLookback(Q,S,r,M):
     h = Q[2]-Q[1]
@@ -47,7 +75,7 @@ def BinomialLookback(Q,S,r,M):
     qu = (np.exp(r*h)-expd)/(expu-expd)
     qd = (expu-np.exp(r*h))/(expu-expd)
     if (qu<0) | (qd<0):
-        display('Error: the market is not arbitrage free.')
+        print('Error: the market is not arbitrage free.')
         price = 0
         return
     [R,Nu] = RandomPathsBinomial(S,M)      #R is M*(N+1), Nu is size M vector
@@ -55,6 +83,18 @@ def BinomialLookback(Q,S,r,M):
     terms = (qu**Nu)*(qd**(N-Nu))*payoff
     price = np.exp(-r*h*N)*sum(terms)*2**N/M    #monte carlo:2^N the possible paths. M samples
     return price
+
+# #######
+# Task 1
+# #######
+
+Q, S = BinomialStock(p, alpha, sigma, S_0, T, N)
+
+
+#Rand,Nu = RandomPathsBinomial(S,50)
+
+#plt.plot(np.transpose(Rand)) #as the arrays are column vectors not row
+#plt.show()
 
 P1 = np.zeros(100)
 P2 = np.zeros(100)
